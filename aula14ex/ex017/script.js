149237cca89700db01d5b248e060d4a4fb6d5b27
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-09-05 12:54:07 -0300

    +Exercício feito (Tabuada) - CursoJavaScript

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,32 @@
-alert('Olá')
+function tabuada() {
+    let num = document.getElementById('numId')
+    let tab = document.getElementById('selectTabId')
+
+    if (num.value == 0) {
+        alert('Por favor, digite um número acima de 0!')
+    } else {
+        let n = Number(num.value)
+        // let c = 1
+        tab.innerHTML = ''
+
+        // COM WHILE
+
+        // while (c <= 10) {
+        //     let item = document.createElement('option')
+        //     item.text = `${n} x ${c} = ${n*c}`
+        //     item.value = `tab${c}`
+        //     tab.appendChild(item)
+        //     c++
+        // }
+
+        // COM FOR
+
+        for (let c = 1; c <= 10; c++) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+    
+}
