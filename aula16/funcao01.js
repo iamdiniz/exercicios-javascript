f2719f3c2fe9fafe41487a1557b86a70143b92b9
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-09-05 16:17:21 -0300

    Funções - Curso JavaScript #16

---
+++ aula16/funcao01.js
@@ -0,0 +1,11 @@
+//              'n' Parâmetro formal
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else { //         Ação
+        return 'Ímpar!'
+    }
+}
+
+//          Chamada
+console.log(parimpar(223)) // '223' parâmetro
